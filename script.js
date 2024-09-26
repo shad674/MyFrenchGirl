cdc9361acef9072c9e2c511c95b984eff2fc26f4
Shad Gabrielle Reyes <75806525+shad674@users.noreply.github.com>
2024-09-26 01:46:28 -0700

    my beloved Agathe

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function videoUrl(video) {
+  if (video === "blopper2.MOV" || video === "blopper4.MOV") {
+    console.log(video);
+    slider.style.objectFit = "cover";
+  } else {
+    slider.style.objectFit = "contain";
+  }
   document.getElementById("slider").src = video;
 }
